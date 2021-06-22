f2896c3cfb40cc782b9c92cf1069e4ed72be14e1
DConstable <d.constable@lancaster.ac.uk>
2021-06-22 15:28:44 +0100

    Markddown generation and locations for talks createdd

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,11 +1,36 @@
 var addressPoints = [
   [
+    "Daresbury, United Kingdom",
+    53.3401912,
+    -2.6329929
+  ],
+  [
+    "Lancaster, United Kingdom",
+    54.0484068,
+    -2.7990345
+  ],
+  [
     "Bangalore, India",
     12.9791198,
     77.5912997
   ],
   [
-    "London,  United Kingdom",
+    "London, United Kingdom",
+    51.5073219,
+    -0.1276474
+  ],
+  [
+    "Didcot, United Kingdom",
+    51.6121773,
+    -1.2439778415871414
+  ],
+  [
+    "Glasgow, United Kingdom",
+    55.8609825,
+    -4.2488787
+  ],
+  [
+    "London, UK",
     51.5073219,
     -0.1276474
   ],
@@ -15,13 +40,13 @@ var addressPoints = [
     12.4829321
   ],
   [
-    "Daresbury, United Kingdom",
-    53.3401912,
-    -2.6329929
-  ],
-  [
     "Kalamata, Greece",
     37.0377582,
     22.1109392
+  ],
+  [
+    "Sendai, Japan",
+    38.2677554,
+    140.8691498
   ]
 ];
